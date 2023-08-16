3c015a174be6b98e026360e6e3e2ea4e72b56fc9
noner <pawel495@interia.pl>
2023-08-16 22:03:15 +0100

    FAB for Prompts Screen

--- app/src/main/java/com/ps/androidgpt/presentation/prompts_screen/PromptsScreen.kt
+++ app/src/main/java/com/ps/androidgpt/presentation/prompts_screen/PromptsScreen.kt
@@ -3,15 +3,17 @@ package com.ps.androidgpt.presentation.prompts_screen
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
@@ -20,7 +22,6 @@ import com.ps.androidgpt.R
 import com.ps.androidgpt.presentation.composables.MyNavigationDrawer
 import com.ps.androidgpt.presentation.composables.MyTopAppBar
 import com.ps.androidgpt.presentation.navigation.Screen
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,6 +41,10 @@ fun PromptsScreen(navController: NavController, drawerState: DrawerState) {
                 title = stringResource(id = R.string.prompts),
                 drawerState = drawerState
             )
+        }, floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
         }, modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)) { padding ->
             LazyColumn(
                 modifier = Modifier.padding(padding)
